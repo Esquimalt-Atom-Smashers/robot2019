f2410e443deac49b1da8a6b4f4c15391b9ce0070
EsqAtomSmashersProgramers <32209970+Sarkhan-the-man@users.noreply.github.com>
2019-03-02 12:02:39 -0800

    Revert "Revert "asdfghjkl""
    This reverts commit c4ff45285ae7944ec384cdbd2fadf4432ae401ba.

--- src/main/java/frc/robot/subsystems/Wrist.java
+++ src/main/java/frc/robot/subsystems/Wrist.java
@@ -1,6 +1,30 @@
 package frc.robot.subsystems;
 
-public class Wrist
-{
+import edu.wpi.first.wpilibj.Encoder;
+import frc.robot.Functions;
+import frc.robot.controlsystems.Joint;
 
+public class Wrist implements Functions
+{
+    private static int TICKS_PER_REV =  1680;
+    public static int VERTICAL = 1680/4;
+    public static int HORIZENTAL = 0;
+
+    private Joint joint;
+
+    public Wrist(int motor, Encoder encoder)
+    {
+        joint = new Joint(motor, encoder, 0.03, 0.000015, 0.004, 40, TICKS_PER_REV, 10000, 1);
+    }
+
+    public void setPosition(int ticks)
+    {
+        //System.out.println();
+        joint.goToPosition(ticks);
+    }
+
+    public void goToPosition()
+    {
+        joint.goToTargetPosition();
+    }
 }
